c03631d61729234bb74440cf8c8633f86bd15952
luuluka <luuluka@local>
2026-08-19 10:03:37 +0800

    fix(pandas): drop nonexistent meson termux package; meson comes via pip meson-python

--- packages/pandas/build.sh
+++ packages/pandas/build.sh
@@ -7,7 +7,6 @@ TERMUX_PKG_VERSION="2.2.3"
 TERMUX_PKG_SRCURL=https://pypi.io/packages/source/p/pandas/pandas-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=4f18ba62b61d7e192368b84517265a99b4d7ee8912f8708660fb4a366cc82667
 TERMUX_PKG_DEPENDS="python, python-pip, python-numpy"
-TERMUX_PKG_BUILD_DEPENDS="meson"
 TERMUX_PKG_PYTHON_COMMON_BUILD_DEPS="meson-python, 'Cython>=3.0'"
 TERMUX_PKG_BUILD_IN_SRC=true
 
